5ebe28de2f5b8c7199538ae50bf5bd397807066d
targamarcello <121225428+targamarcello@users.noreply.github.com>
2023-03-06 15:31:22 +0000

    Add files via upload

---
+++ frequenza dei caratteri.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrequenzaCaratteri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tasto, contCifre = 0, contCaratteri = 0;
+            int[] cont = new int[26];
+            Console.WriteLine("Inserisci frase, tutta maiuscola");
+            
+            do
+            {
+                tasto = Convert.ToInt32(Console.Read());
+                if (tasto>=65 && tasto <= 90)
+                {
+                    for(int i=0; i<cont.Length; i++)
+                    {
+                        if (tasto == 65 + i)
+                        {
+                            cont[i]++;
+                        }
+                    }
+                }else if(tasto>=48 && tasto <= 57)
+                {
+                    contCifre++;
+                }
+                else
+                {
+                    contCaratteri++;
+                }
+            } while (tasto != 13);
+            for(int i=0; i < cont.Length; i++)
+            {
+                if (cont[i] != 0)
+                {
+                    Console.WriteLine($"la lettera {Convert.ToChar(65 + i)} appare {cont[i]} volte");
+                }
+            }
+            Console.WriteLine($"il carattere appare {contCaratteri} volte");
+            Console.WriteLine($"la cifra appare {contCifre} volte");
+            Console.ReadLine();
+            Console.ReadLine();
+        }
+    }
+}
